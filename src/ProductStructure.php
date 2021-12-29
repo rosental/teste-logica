534a9b4845e5ff53477ee46d4125d7d621abf66b
rosental <cristiane.rosental@gmail.com>
2021-12-29 00:22:55 -0300

    Updating ProductStructure.php.

--- src/ProductStructure.php
+++ src/ProductStructure.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App;
 
 class ProductStructure
@@ -18,9 +19,36 @@ class ProductStructure
         "azul-P"
     ];
 
+    /**
+     * Returns an array with the product quantity for each size by color.
+     * @param array $products Products array with each item formated like: "color-size"
+     * @param string $separator The separator used between the item's color and size.
+     * @return array
+     */
+    private function getProductsQtyByColorAndSize(array $products, string $separator = "-"): array
+    {
+        $products_qty = [];
+
+        foreach ($products as $product) {
+
+            // Gets product info (Color and Size)
+            $product_info = explode($separator, $product);
+            $color = $product_info[0];
+            $size = $product_info[1];
+
+            // If the qty for the size variant already exists, increments it. Otherwise creates a new entry.
+            if (!empty($products_qty[$color][$size])) {
+                $products_qty[$color][$size]++;
+            } else {
+                $products_qty[$color][$size] = 1;
+            }
+        }
+
+        return $products_qty;
+    }
+
     public function make(): array
     {
-       //todo your code.
-        return [];
+        return $this->getProductsQtyByColorAndSize(self::products);
     }
 }
